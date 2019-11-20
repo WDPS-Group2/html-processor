ec193a949bb895019bedad404e58c856b2a4d3e4
Corneliu Soficu <corneliu.soficu@gmail.com>
2019-11-20 14:44:16 +0100

    Created class for the html stripping part

---
+++ html-processor.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+import requests
+
+class HtmlProcessor:
+    
+    PARSER_TYPE = 'html.parser'
+
+    @staticmethod
+    def extract_raw_text_from_html(html_content):
+        """ Strips away all the html tags including the script and style content """
+
+        parsed_html = BeautifulSoup(html_content, HtmlProcessor.PARSER_TYPE)
+
+        for script in parsed_html(['script', 'style']):
+            script.decompose()
+
+        raw_text = parsed_html.get_text()
+        
+        lines = (line.strip() for line in raw_text.splitlines())
+        chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+        raw_text = '\n'.join(chunk for chunk in chunks if chunk)
+
+        return raw_text
+
+    
+    @staticmethod
+    def extract_raw_text_from_url(url):
+        response = requests.get(url)
+        if response.status_code == 200:
+            return HtmlProcessor.extract_raw_text_from_html(response.text)
+        else:
+            print("Could not get webpage, status code: %d" % response.status_code)
+            return ""
+    
+
+if __name__ == "__main__":
+    url = "https://www.timeout.com/amsterdam/things-to-do/best-things-to-do-in-amsterdam"
+    raw_text = HtmlProcessor.extract_raw_text_from_url(url)
+    print(raw_text)
